88c10ce60722fbd43c0e8e384c4119ffb6c24da1
GK0610 <gk120000ks@gmail.com>
2025-03-01 16:10:47 +0530

    Configured GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import UserForm from "./components/UserForm";
 
 const App: React.FC = () => {
