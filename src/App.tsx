d7079015d0e4059422b1fab3b10761bbb5cde5bb
GK0610 <gk120000ks@gmail.com>
2025-03-01 16:27:54 +0530

    Configured GitHub Pages deployment

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App: React.FC = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/user_form_googleapi/" element={<UserForm />} />
+        <Route path="/" element={<UserForm />} />
       </Routes>
     </Router>
   );
